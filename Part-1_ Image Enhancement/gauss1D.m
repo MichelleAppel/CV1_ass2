e9d1747e62ab0a9ce19ab78fe73eed34642b7b19
Michelle <appel.michelle@gmail.com>
2018-02-24 17:35:52 +0100

    gauss1D function

--- Part-1_ Image Enhancement/gauss1D.m
+++ Part-1_ Image Enhancement/gauss1D.m
@@ -4,4 +4,14 @@ function G = gauss1D( sigma , kernel_size )
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
+    f = -floor(kernel_size/2);
+    n = floor(kernel_size/2);
+    
+    % Calculate gaussian for every x between f and n
+    for x = f:n
+        G(x+floor(kernel_size/2)+1)=(1/(sigma*(sqrt(2*pi))))*exp(-(x^2)/(2*sigma^2));
+    end
+    
+    % Normalize
+    G = G/sum(G);
 end
