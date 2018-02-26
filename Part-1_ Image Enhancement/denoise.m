8358b1e3d1da7a2576771dc2d77af14b6f58505b
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-02-26 19:20:06 +0100

    box, median, gaussian filtering

--- Part-1_ Image Enhancement/denoise.m
+++ Part-1_ Image Enhancement/denoise.m
@@ -1,11 +1,37 @@
 function [ imOut ] = denoise( image, kernel_type, varargin)
 
+clc % clear command window
+
+switch nargin
+    case 2
+        disp('2 inputs given')
+        kernel_size = 3;            % default
+        sigma = 1;                  % default
+    case 3
+        disp('3 inputs given')
+        kernel_size = varargin{1};  % first arg
+        sigma = 1;                  % default
+    case 4
+        disp('4 inputs given')
+        kernel_size = varargin{1};  % first arg
+        sigma = varargin{2};        % second arg
+end
+
+imName = char(strcat('gen_images/', image(15:22), '_', ...
+    kernel_type, '_', string(kernel_size), '.jpg'));
+image = imread(image);
+
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        imOut = imboxfilt(image, kernel_size);
     case 'median'
-        fprintf('Not implemented\n')
+        imOut = medfilt2(image, [kernel_size kernel_size]);
     case 'gaussian'
-        fprintf('Not implemented\n')
+        gaussFilter = gauss2D(sigma, kernel_size);
+        imOut = imfilter(image, gaussFilter);
 end
+
+imshow(imOut);
+imwrite(imOut, imName);
+
 end
