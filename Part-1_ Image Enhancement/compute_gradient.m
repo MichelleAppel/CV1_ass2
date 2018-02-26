2bf69f67cb7aad40a8924201eb3f102b12a3f286
Michelle <appel.michelle@gmail.com>
2018-02-26 22:41:10 +0100

    implemented

--- Part-1_ Image Enhancement/compute_gradient.m
+++ Part-1_ Image Enhancement/compute_gradient.m
@@ -1,5 +1,10 @@
-function [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image)
-fprintf('Not implemented\n')
+function [Gx, Gy, im_magnitude, im_direction] = compute_gradient(image)
+	image = imread(image);
 
+    sobel_kernel = [1, 0, -1; 2, 0, -2; 1, 0, -1];
+    Gx = conv2(sobel_kernel, image);
+    Gy = conv2(sobel_kernel', image);
+    
+    im_magnitude = sqrt(Gx.^2 + Gy.^2);
+    im_direction = atan(Gy./Gx);
 end
-
