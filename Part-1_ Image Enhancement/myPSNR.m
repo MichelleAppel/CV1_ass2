2f5abf7ab8ec81328b499521e85d6bed256f865f
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-02-26 18:19:16 +0100

    PSNR

--- Part-1_ Image Enhancement/myPSNR.m
+++ Part-1_ Image Enhancement/myPSNR.m
@@ -1,5 +1,6 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
+
+PSNR = 20 * log10(double(max(max(orig_image)) / sqrt(immse(orig_image, approx_image))));
 
 end
 
