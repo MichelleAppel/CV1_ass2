48ecdbc7ce5477a73391120f59c0882b368f7e2e
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-02-26 13:05:38 +0100

    cosCarrier, sinCarrier, gaussEnv

--- Part-2_ Gabor Segmentation/createGabor.m
+++ Part-2_ Gabor Segmentation/createGabor.m
@@ -58,16 +58,18 @@ sinCarrier = createSin(rot_x, lambda, psi);
 
 % Modulate (multiply) Gaussian envelope with the carriers to compute 
 % the real and imaginary components of the omplex Gabor filter. 
-myGabor_real = 1;    % \\TODO: modulate gaussianEnv with cosCarrier
-myGabor_imaginary = 1; % \\TODO: modulate gaussianEnv with sinCarrier
+%myGabor_real = 1;    % \\TODO: modulate gaussianEnv with cosCarrier
+%myGabor_imaginary = 1; % \\TODO: modulate gaussianEnv with sinCarrier
+myGabor_real = gaussianEnv * cosCarrier;
+myGabor_imaginary = gaussianEnv * sinCarrier;
 
 % Pack myGabor_real and myGabor_imaginary into myGabor.
 myGabor(:,:,1) = myGabor_real;
 myGabor(:,:,2) = myGabor_imaginary;
 
-% figure;
-% subplot(121), imshow(myGabor_real,[]);
-% subplot(122), imshow(myGabor_imaginary, []);
+figure;
+subplot(121), imshow(myGabor_real,[]);
+subplot(122), imshow(myGabor_imaginary, []);
 end
 
 
@@ -84,7 +86,8 @@ end
 function cosCarrier = createCos(rot_x, lambda, psi)
 % ----------------------------------------------------------
 % Returns the 2D cosine carrier. 
-cosCarrier = 1; % \\TODO: Implement the cosine given rot_x, lambda and psi.
+%cosCarrier = 1; % \\TODO: Implement the cosine given rot_x, lambda and psi.
+cosCarrier = cos(2*pi*rot_x/lambda + psi);
 
 % Reshape the vector representation to matrix.
 cosCarrier = reshape(cosCarrier, sqrt(length(cosCarrier)), []);
@@ -94,7 +97,8 @@ end
 function sinCarrier = createSin(rot_x, lambda, psi)
 % ----------------------------------------------------------
 % Returns the 2D sine carrier. 
-sinCarrier = 1% \\TODO: Implement the sine given rot_x, lambda and psi.
+%sinCarrier = 1% \\TODO: Implement the sine given rot_x, lambda and psi.
+sinCarrier = sin(2*pi*rot_x/lambda + psi);
 
 % Reshape the vector representation to matrix.
 sinCarrier = reshape(sinCarrier, sqrt(length(sinCarrier)), []);
@@ -104,7 +108,8 @@ end
 function gaussEnv = createGauss(rot_x, rot_y, gamma, sigma)
 % ----------------------------------------------------------
 % Returns the 2D Gaussian Envelope. 
-gaussEnv = 1;% \\TODO: Implement the Gaussian envelope.
+%gaussEnv = 1;% \\TODO: Implement the Gaussian envelope.
+gaussEnv = exp((rot_x^2 + gamma^2 * rot_y^2) / 2*sigma^2);
 
 % Reshape the vector representation to matrix.
 gaussEnv = reshape(gaussEnv, sqrt(length(gaussEnv)), []);
