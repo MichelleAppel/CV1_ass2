c537a389be88f88c2fcd01410996220dca70be8c
Michelle <appel.michelle@gmail.com>
2018-02-27 20:05:11 +0100

    LoG kerne

--- Part-1_ Image Enhancement/compute_LoG.m
+++ Part-1_ Image Enhancement/compute_LoG.m
@@ -1,18 +1,21 @@
 function imOut = compute_LoG(image, LOG_type)
+image = imread(image);
 
 switch LOG_type
     case 1
         %method 1
-        fprintf('Not implemented\n')
-
+        G2D = gauss2D(0.5, 5);
+        im = imfilter(image, G2D);
+        [~, ~, ~, im_direction] = compute_gradient(im);
+        [~, ~, ~, imOut] = compute_gradient(im_direction);
+        
     case 2
         %method 2
-        fprintf('Not implemented\n')
-
+        LoG1D = 
+        
     case 3
         %method 3
         fprintf('Not implemented\n')
 
 end
 end
-
--- Part-1_ Image Enhancement/compute_gradient.m
+++ Part-1_ Image Enhancement/compute_gradient.m
@@ -1,9 +1,9 @@
 function [Gx, Gy, im_magnitude, im_direction] = compute_gradient(image)
-	image = imread(image);
+	%image = imread(image);
 
     sobel_kernel = [1, 0, -1; 2, 0, -2; 1, 0, -1];
-    Gx = conv2(sobel_kernel, image);
-    Gy = conv2(sobel_kernel', image);
+    Gx = double(imfilter(image, sobel_kernel));
+    Gy = double(imfilter(image, sobel_kernel'));
     
     im_magnitude = sqrt(Gx.^2 + Gy.^2);
     im_direction = atan(Gy./Gx);
